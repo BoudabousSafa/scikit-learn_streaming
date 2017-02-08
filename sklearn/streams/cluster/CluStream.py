b43024da856ff6f767ec451fd39b6a4d856e901c
oumoussmehdi <oumoussmehdi@hotmail.com>
2017-02-08 14:43:25 +0100

    Canvas

---
+++ sklearn/streams/cluster/CluStream.py
@@ -0,0 +1,46 @@
+from sklearn.base import BaseEstimator, ClusterMixin
+from sklearn.utils import check_array
+from sklearn.neighbors import NearestNeighbors
+import math as math
+import numpy as np
+
+
+class DenStream(BaseEstimator, ClusterMixin):
+    """Implementation of DenStream
+       Read more in the :
+       Parameters
+       ----------
+       Examples
+       --------
+       * >>> X =
+       * >>> y =
+       * >>> from sklearn.xxx import DenStream
+       * >>> ds = DenStream(params)
+       * >>> ds.fit(X, y) # doctest: +ELLIPSIS
+       DenStream(...)
+       * >>> print(ds.predict())
+       See also
+       --------
+       Notes
+       -----
+       """
+
+    def __init__(self):
+        
+
+    def fit(self, X, Y=None):
+       
+
+    def predict(self, X):
+        """Predict the class labels for the provided data
+        Parameters
+        ----------
+        X :
+        Returns
+        -------
+        y :
+        """
+        
+        knn = nearestNeighbour()
+        result = knn.fit_predict(cluster_centers, None)
+        return result
